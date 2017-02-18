9399ae2d129dac4e6ccf0a3b36c3bc0b7b1c3a9a
Aidan Markham <apm4337@g.rit.edu>
2017-02-18 14:36:21 -0500

    Organized

---
+++ Assets/Scripts/CameraFly.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFly : MonoBehaviour {
+    public float speed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += GameObject.Find("Main Camera").transform.forward * Input.GetAxisRaw("Vertical") * speed * Time.deltaTime;
+        transform.position += GameObject.Find("Main Camera").transform.right * Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime;
+    }
+}
