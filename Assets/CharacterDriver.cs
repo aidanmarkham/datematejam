73cbb84a9a493dbd731d208f6f7dff97e675851f
Aidan Markham <apm4337@g.rit.edu>
2017-02-19 14:26:41 -0500

    Deer animations and walking

---
+++ Assets/CharacterDriver.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CharacterDriver : MonoBehaviour {
+    public float moveSpeed;
+    public GameObject cameraRig;
+    public CharacterController characterController;
+    private Quaternion lookDir;
+    public string horizontalAxis;
+    public string verticalAxis;
+    private float speed;
+    public float animSpeed;
+    private Animator animator;
+    
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        characterController.Move(cameraRig.transform.TransformDirection( new Vector3(Input.GetAxis(horizontalAxis), 0, Input.GetAxis(verticalAxis))) * moveSpeed * Time.deltaTime);
+        speed = cameraRig.transform.TransformDirection(new Vector3(Input.GetAxis(horizontalAxis), 0, Input.GetAxis(verticalAxis))).magnitude * moveSpeed * Time.deltaTime;
+
+        lookDir = Quaternion.LookRotation(cameraRig.transform.TransformDirection(new Vector3(Input.GetAxis(horizontalAxis), 0, Input.GetAxis(verticalAxis))));
+        
+        if(Input.GetAxis(horizontalAxis) != 0 || Input.GetAxis(verticalAxis) != 0)
+        {
+            transform.rotation = lookDir;
+        }
+        animator.speed = speed * animSpeed;
+        
+    }
+}
---
+++ Assets/Scripts/CameraRigCenter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRigCenter : MonoBehaviour {
+    public GameObject player1;
+    public GameObject player2;
+    public Camera cam;
+    public Vector3 targetPosition;
+    public float speed;
+    public float furthestDistance;
+    public float minDistance;
+    public float maxDistance;
+    public float distanceScale;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        targetPosition = (player1.transform.position + player2.transform.position) / 2;
+
+        transform.position += (targetPosition - transform.position) * speed;
+
+        
+        
+        furthestDistance = (player1.transform.position - transform.position).magnitude;
+
+        furthestDistance = Mathf.Clamp(furthestDistance, minDistance, maxDistance);
+        cam.transform.localPosition = new Vector3(0, furthestDistance * distanceScale, 0);
+    }
+}
